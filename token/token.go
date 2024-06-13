3cc4fdd43cf22286937fef3ef2c66cb29812ba8f
Biswajit Oraon <biswajitoraon19@gmail.com>
2024-06-14 03:50:42 +0530

    chore(lexer): initial lexer description

---
+++ token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	//identifiers + tokens
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	//operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	//delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
